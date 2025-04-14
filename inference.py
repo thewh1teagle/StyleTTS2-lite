219699bba8bef46218072228f6e01ad09a38e318
dangtr0408 <dangtr0408@gmail.com>
2025-04-14 11:55:06 +0700

    fixing 1AM bug

--- inference.py
+++ inference.py
@@ -323,7 +323,7 @@ class StyleTTS2(torch.nn.Module):
             text = re.sub(lang_pattern, replacement_func, text)
 
         texts = re.split(r'(\[id_\d+\])', text) #split the text by speaker ids while keeping the ids.
-        if len(texts) <= 1 or bool(re.match(r'(\[id_\d+\])', texts[0])): #Add a default speaker
+        if len(texts) <= 1 or bool(re.match(r'(\[id_\d+\])', texts[0])) == False: #Add a default speaker
             texts.insert(0, default_speaker)
         curr_id = None
         for i in range(len(texts)): #remove consecutive ids
