c4525d1e151023cd1d6feccc7f9daad5d7466390
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-05-20 02:01:21 +0300

    feat: add onnx run script and enhance export

---
+++ Demo/infer.py
@@ -0,0 +1,45 @@
+"""
+uv pip install -r ./Demo/requirements.txt
+uv pip install pandas
+uv run Demo/infer.py
+"""
+import soundfile as sf
+import sys
+import torch
+import numpy as np
+from pathlib import Path
+
+
+root_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(root_dir))
+from inference import StyleTTS2
+
+
+
+def main():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu' #setup GPU
+    config_path = str(Path("Configs") / "config.yaml")
+    models_path = str(Path('Models') / 'Finetune/base_model.pth')
+    model = StyleTTS2(config_path, models_path).eval().to(device)
+    
+    phonemes = 'həlˈO wˈɜɹld!'
+    speed = 1
+    denoise = 0.2
+    avg_style = True
+    stabilize = True
+
+    speaker = {
+        "path": 'Demo/Audio/10_michael.wav',
+        "speed": speed
+    }
+
+    with torch.no_grad():
+        styles = model.get_styles(speaker, denoise, avg_style)
+        r = model.generate(phonemes, styles, stabilize, 18)
+        r = r / np.max(np.abs(r))  # Normalize
+
+    sr = 24000
+    sf.write("audio.wav", r, sr)
+    print('Created audio.wav')
+    
+main()
--- ONNX/test.py
+++ ONNX/test.py
@@ -3,14 +3,16 @@ uv sync
 wget https://huggingface.co/dangtr0408/StyleTTS2-lite/resolve/main/Models/base_model.pth
 wget https://huggingface.co/dangtr0408/StyleTTS2-lite/resolve/main/Models/config.yaml
 
-uv pip install onnx espeakng-loader phonemizer-fork
+uv pip install onnx espeakng-loader phonemizer-fork onnxruntime
 uv run ONNX/export_onnx.py
 """
 import torch
 from inference_onnx import StyleTTS2, Preprocess
 import sys
-import phonemizer
+import onnxruntime
 from pathlib import Path
+import phonemizer
+import soundfile as sf
 
 if sys.platform in ['win32', 'darwin']:
     from phonemizer.backend.espeak.wrapper import EspeakWrapper
@@ -36,34 +38,27 @@ speaker = {
 }
 text = 'Nearly 300 scholars currently working in the United States have applied for positions at Aix Marseille University in France.'
 
-def export():
+def main():
     with torch.no_grad():
-        model = StyleTTS2(config_path, models_path)
         preprocess = Preprocess(config_path, models_path)
 
         phonemes = get_phoneme(text=text, lang="en-us")
         tokens, mel, speed = preprocess.preprocess_input(phonemes, speaker)
         style              = preprocess.get_style(mel)
         model_inputs = {
-            "tokens": tokens,
-            "style": style,
-            "speed": speed,
+            "tokens": tokens.numpy(),
+            "style": style.numpy(),
+            "speed": speed.numpy(),
         }
-        input_names = ['tokens', 'style', 'speed']
-        torch.onnx.export(model, kwargs=model_inputs,
-                                f='model.onnx', 
-                                dynamo=False, export_params=True, report=False, verify=True,
-                                input_names=input_names,
-                                output_names=["output_wav"],
-                                training=torch.onnx.TrainingMode.EVAL,
-                                opset_version=19,
-                                dynamic_axes={
-                                    'tokens':{0: "num_token"},
-                                    'output_wav':{0: "wav_length"}
-                                }
-                        )
+        
+        sess = onnxruntime.InferenceSession("model.onnx")
+        wav = sess.run(None, model_inputs)[0]
 
-    print('Onnx export complete! ✅')
+        sf.write('audio.wav', wav, 24000)
+        print('Created audio.wav')
+
+    
 
 if __name__ == "__main__":
-    export()
+    main()
+
